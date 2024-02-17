f865f3db6338b70cdb20075074e7636284be6fa8
DALI <sika.dali@telecomnancy.net>
2024-02-17 23:50:35 +0100

    api call and result

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-let url = `https://v6.exchangerate-api.com/v6/8c2bfe90fab0a4bfbbddcbf6/pair/USD/EUR`;
+import { apiKey } from "./api-key.js"; // i have my API keys there
+const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/`;
 
 const convertButton = document.getElementById("convert");
 
-convertButton.addEventListener("click", () => {});
+convertButton.addEventListener("click", () => {
+     let firstNumber = Number(document.getElementById("first").value);
+
+     if (isNaN(firstNumber)) {
+          alert("THE ENTRY IS NOT A NUMBER, FIX IT");
+     } else {
+          let pair = `EUR/USD`;
+          console.log("CLICK CONVERT " + firstNumber);
+          apiCall(url + pair, firstNumber);
+     }
+});
+
+function apiCall(url, firstNumber) {
+     fetch(url)
+          .then((response) => {
+               return new Promise((resolve) =>
+                    response.json().then((json) =>
+                         resolve({
+                              status: response.status,
+                              ok: response.ok,
+                              json,
+                         })
+                    )
+               );
+          })
+          .then(({ status, json, ok }) => {
+               console.log(json);
+               switch (status) {
+                    case 404:
+                         alert("CURRENCY NOT FOUND, TRY AGAIN");
+                         break;
+                    case 200:
+                         console.log("YIPPI !!");
+                         console.log("RESULT " + json["conversion_rate"]);
+                         break;
+                    default:
+                         handleUnexpected(status, json);
+               }
+          });
+}
+
+function handleUnexpected(status, json) {
+     alert(status + ": " + json["error-type"]);
+}
